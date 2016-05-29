272e4c649867314902ac0efe970380751259d5a3
Lenlee <lenlee.lijia@outlook.com>
2016-05-29 22:45:58 +0800

    last updated

--- Python3_3.4Dict001.py
+++ Python3_3.4Dict001.py
@@ -1,25 +1,36 @@
+''' Some description about the codes'''
+
 #!/usr/bin/env python3
-signals= {'green':'go','yellow':'go faster','red':'smile for the camera'}
-print(list(signals.keys()))
-print(list(signals.values()))
-print(list(signals.items()))
-empty_set=set()
-print(empty_set)
-even_numbers={0,2,4,6,8.0,10,12.0}
-odd_numbers={1,3,5.0,7.0,9.0,11,13.0}
-print(even_numbers)
-print(odd_numbers)
+
+import time
+SIGNALS = {'green':'go', 'yellow':'go faster', 'red':'smile for the camera'}
+print(list(SIGNALS.keys()))
+print(list(SIGNALS.values()))
+print(list(SIGNALS.items()))
+EMPTY_SET = set()
+print(EMPTY_SET)
+EVEN_NUM = {0, 2, 4, 6, 8.0, 10, 12.0}
+ODD_NUM = {1, 3, 5.0, 7.0, 9.0, 11, 13.0} 
+print(EVEN_NUM)
+print(ODD_NUM)
 print(set('letters'))
-""" Start the in function in the dictionary data"""
-drinks= {
-    'martini':{'vodka','vermouth'},
-    'black russian':{'vodka','kahlua'},
-    'white russian':{'cream','kahlua','vodka'},
-    'manhattan':{'rye','vermouth','bitters'},
-    'screwdriver':{'orange juice','vodka'}
+###Start the in function in the dictionary data
+DRINKS = {
+    ' martini': {'vodka', 'vermouth'},
+    'black russian':{'vodka', 'kahlua'},
+    'white russian':{'cream', 'kahlua', 'vodka'},
+    'manhattan':{'rye', 'vermouth', 'bitters'},
+    'screwdriver':{'orange juice', 'vodka'}
 }
-for name,contents in drinks.items():
+for name, contents in DRINKS.items():
     if 'vodka' in contents:
         print(name)
 
-import this
+'''import this'''
+
+
+NOW = time.time()
+print(time.ctime(NOW))
+print(time.localtime(NOW))
+
+time = time.time()
